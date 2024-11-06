8597194990ed49f6bb3c5e07f5d389eb2a62fdb5
ЕвгенийК <widgetev@gmail.com>
2024-11-06 13:18:17 +0300

    Init commit

---
+++ src/main/java/com/example/propracticfinal/ProPracticFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.propracticfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPracticFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProPracticFinalApplication.class, args);
+    }
+
+}
